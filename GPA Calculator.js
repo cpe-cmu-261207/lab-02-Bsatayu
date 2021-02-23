0bdc36afac8f728e01acaab3819d7e75150514d0
Bsatayu <satayu_s@cmu.ac.th>
2021-02-23 15:21:08 +0700

    commit

--- GPA Calculator.js
+++ GPA Calculator.js
@@ -1,73 +1,56 @@
-document.addEventListener('DOMContentLoaded',function(){
-    document.querySelector('form').onsubmit = function(){
-        const semester = document.querySelector('#semester').value
-        const table = document.getElementById(semester)
-        const newRow = document.createElement('tr')
-        const subjCol = document.createElement('td')
-        const delCol = document.createElement('td')
-        const weightCol = document.createElement('td')
-        const delBtn = document.createElement('button')
-        const gradeCol = document.createElement('td')
+var s =[];//subject
+var c =[];//code
+var t =[];//term
+var u =[];//unit
+var g =[];//grade
+var n = 0;
 
-        delBtn.innerHTML = 'x'
-        delBtn.onclick = function(){
-            table.removeChild(newRow)
-        }
-        let grade = document.createElement('span')
-        grade = document.getElementById("gpa")
-        let g = grade.options[grade.selectedIndex].text
-        let weight = document.createElement('span')
-        weight = document.querySelector("#weight").value
-        let subjID = document.createElement("span")
-        subjID = document.querySelector("#subID").value
-        subjCol.append(subjID)
-        delCol.append(delBtn)
-        weightCol.append(weight)
-        gradeCol.append(g)
-        newRow.append(subjCol)
-        newRow.append(weightCol)
-        newRow.append(gradeCol)
-        newRow.append(delCol)
-        table.append(newRow)
-        
-        var newList = (Number(grade.value) * Number(weight))
+function newSub(){
+    var allSub = document.querySelector('#allSub');
+        var subject = document.querySelector('#subject');
+        var code = document.querySelector('#code');
+        var term = document.querySelector('#term');
+        var unit = document.querySelector('#unit');
+        var grade = document.querySelector('#grade');
 
-        //per semester
-        var gpasmt = "gpa" + semester
-        var wsmt = "w" + semester
-        var gpaPerSmt = document.getElementById(gpasmt).innerHTML
-        var wPerSmt = document.getElementById(wsmt).innerHTML
-        wPerSmt = Number(wPerSmt) + Number(weight)
-        gpaPerSmt = Number(gpaPerSmt)
-        gpaPerSmt = ((gpaPerSmt * (wPerSmt - weight)) + newList) / wPerSmt
-        document.getElementById(gpasmt).innerHTML = String(gpaPerSmt)
-        document.getElementById(wsmt).innerHTML = String(wPerSmt)
+        var line = document.createElement('li');
+        var text = document.createElement('span');
+        text.innerHTML = subject.value +"("+code.value+") term"+term.value+" unit"+unit.value+" Grade "+grade.value;
+    
+        line.append(text);
+        allSub.append(line);
 
-        //gpax
-        var gpax = document.getElementById("gpax").innerHTML
-        gpax = Number(gpax)
-        var wx = document.getElementById("wx").innerHTML
-        wx = Number(wx)
-        gpax = Number(gpax)  * Number(wx)
-        wx = Number(wx) + Number(weight)
-        gpax = (Number(gpax) + Number(newList))/Number(wx)
-        document.getElementById("gpax").innerHTML = String(gpax)
-        document.getElementById("wx").innerHTML = String(wx)
+        //store data
+        s[n] = subject.value;
+        c[n] = parseInt(code.value);
+        t[n] = term.value;
+        u[n] = parseInt(unit.value);
+        g[n] = parseFloat(grade.value);
+        n++;
+        //clear input
+        subject.value ='';
+        code.value ='';
+        term.value ='';
+        unit.value ='';
+        grade.value ='';
+}
 
-        //gpaCPE
-        var subjCheck = subjID.substr(0,3)
-        if(subjCheck == "261" || subjCheck == "269"){
-            var gpaComp = document.getElementById("gpacpe").innerHTML
-            var wComp = document.getElementById("wcpe").innerHTML
-            gpaComp = Number(gpaComp)
-            wComp = Number(wComp)
-            gpaComp = gpaComp * wComp
-            wComp = wComp + Number(weight)
-            gpaComp = (gpaComp + newList)/wComp
-            document.getElementById("gpacpe").innerHTML = String(gpaComp)
-            document.getElementById("wcpe").innerHTML = String(wComp)
-        }
-
-        return false
+function calAll(){
+    var num = 0; var unitAll=0;
+    for(var i=0;i<n;i++){
+        num+=g[i]*u[i];
+        unitAll+=u[i];
     }
-})
+    alert((num/unitAll).toFixed(2));
+}
+
+function calMajor(){
+    var num = 0; var unitAll=0;
+    for(var i=0;i<n;i++){
+        if(Math.floor(c[i]/1000) == 261 || Math.floor(c[i]/1000) == 269){
+            num+=g[i]*u[i];
+            unitAll+=u[i];
+        }
+    }
+    alert((num/unitAll).toFixed(2));
+}
